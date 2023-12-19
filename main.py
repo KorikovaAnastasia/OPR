07bc462478a7e22c7ab0de1f162f5022fcd637f8
an.vokirok <61110053+KorikovaAnastasia@users.noreply.github.com>
2023-12-19 12:02:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -28,31 +28,26 @@ def clicked():
     result_output.place(x=0, y=400)
 
     if selected.get() == 1:
-
         res = secant_method(f1, value_x0, value_x1, value_eps, it)
-        output_result = "ответ: {}".format(res)
-        result_output.configure(text=output_result)
+        output_result = "Ответ: {}".format(res)
+        result_output.configure(text=output_result, font=("Arial", 14, "bold"))
         plot_function(f1, [value_x0, value_x1])
-
     elif selected.get() == 2:
         res = secant_method(f2, value_x0, value_x1, value_eps, it)
-        output_result = "ответ: {}".format(res)
-        result_output.configure(text=output_result)
+        output_result = "Ответ: {}".format(res)
+        result_output.configure(text=output_result, font=("Arial", 14, "bold"))
         plot_function(f2, [value_x0, value_x1])
-
     elif selected.get() == 3:
         res = secant_method(f3, value_x0, value_x1, value_eps, it)
-        output_result = "ответ: {}".format(res)
-        result_output.configure(text=output_result)
+        output_result = "Ответ: {}".format(res)
+        result_output.configure(text=output_result, font=("Arial", 14, "bold"))
         plot_function(f3, [value_x0, value_x1])
-
     else:
-        result_output.configure(text='вы должны выбрать функцию!')
-
+        result_output.configure(text='Вы должны выбрать функцию!')
 
 window = Tk()
 window.title("Метод секущих")
-window.geometry('720x450')  # указание размеров окна
+window.geometry('720x450')
 
 h1 = Label(window, text="Метод секущих", font=("Arial", 16, "bold"))
 h1.place(x=30, y=18, anchor="w")
@@ -72,23 +67,27 @@ canvas = FigureCanvasTkAgg(fig, master=window)
 canvas.draw()
 canvas.get_tk_widget().place(x=200, y=10)
 
-
 enter_x0 = Entry(window, width=10)
 enter_x0.place(x=80, y=40)
 enter_x1 = Entry(window, width=10)
 enter_x1.place(x=80, y=80)
-#
 enter_eps = Entry(window, width=10)
-#
 enter_eps.place(x=80, y=120)
 
-enter_x0.focus()  # для автоматического наведения на окошко ввода
+enter_x0.focus()
+
+enter_x0.insert(0, "0")
+enter_x1.insert(0, "1")
+enter_eps.insert(0, "0.001")
 
 selected = IntVar()
 
 rad1 = Radiobutton(window, text='x^2 - 4', value=1, variable=selected)
 rad2 = Radiobutton(window, text='x + 5', value=2, variable=selected)
-rad3 = Radiobutton(window, text='sin(x)', value=3, variable=selected)
+rad3 = Radiobutton(window, text='2x^3 + 7', value=3, variable=selected)
+
+rad1.select()
+
 rad1.place(x=10, y=180, anchor="w")
 rad2.place(x=10, y=205, anchor="w")
 rad3.place(x=10, y=230, anchor="w")
@@ -107,26 +106,37 @@ def f2(x):
 
 
 def f3(x):
-    return sin(x)
+    return 2 * x**3 + 7
 
 
 def secant_method(f, x0, x1, epsilon, max_iterations):
-
-    # Инициализация значений
     x_prev = x0
     x_curr = x1
     iterations = 0
+    x_range = [x_prev, x_curr]
+    x = np.linspace(x_range[0], x_range[1], 100)
+    y = f(x)
+
+    fig = plt.figure(figsize=(5, 4), dpi=100)
+    ax = fig.add_subplot(111)
+    ax.plot(x, y)
+
+    x_prev = x_range[0]
+    x_curr = x_range[1]
+    secant_x = np.array([x_prev, x_curr])
+    secant_y = f(secant_x)
+    ax.plot(secant_x, secant_y, color='red', linestyle='--')
+
+    canvas = FigureCanvasTkAgg(fig, master=window)
+    canvas.draw()
+    canvas.get_tk_widget().place(x=200, y=10)
 
     while abs(f(x_curr)) > epsilon and iterations < max_iterations:
-        # Вычисление значения следующей итерации метода секущих
         x_next = x_curr - (f(x_curr) * (x_curr - x_prev)) / (f(x_curr) - f(x_prev))
-
-        # Обновление значений для следующей итерации
         x_prev = x_curr
         x_curr = x_next
         iterations += 1
 
-    # Проверка, достигнута ли допустимая погрешность или максимальное количество итераций
     if abs(f(x_curr)) <= epsilon:
         return x_curr
     else:
